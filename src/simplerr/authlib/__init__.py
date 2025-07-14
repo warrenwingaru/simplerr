5fbcf7cca21c541c172f78e31f4d16030708e4c7
Warren Angelo H. Layson <warrenlayson@gmail.com>
2025-07-14 08:18:09 +0800

    fix: add fallback for importlib.metadata
    - Added fallback to `importlib_metadata` for compatibility with older Python versions.

--- src/simplerr/authlib/__init__.py
+++ src/simplerr/authlib/__init__.py
@@ -1,4 +1,7 @@
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 required_version = "1.2.1"
 try:
